bf6f2c73b5531db9c7e70d4286a4ffcc8f58413d
yagya prakash <prakashyagya@gmail.com>
2020-03-12 19:25:11 +0530

    Updates

---
+++ precache-manifest.17f646f0cddc597a095c75a8469cb158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99771860243c5b6a6ae4e8bc8db2c142",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa3a712bf4498636e33c",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a682fe5b3805a5997980",
+    "url": "/portfolio/static/js/2.fd9d23f7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/portfolio/static/js/2.fd9d23f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3a712bf4498636e33c",
+    "url": "/portfolio/static/js/main.e2cf4427.chunk.js"
+  },
+  {
+    "revision": "db97f743f67e9f9a0bde",
+    "url": "/portfolio/static/js/runtime-main.a6bf5a50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
